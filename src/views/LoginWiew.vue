c4499c61d6a8e48183501a2450a1d00539f6c191
Adrian Sitaru <101205953+AdrianSitaru@users.noreply.github.com>
2023-03-17 13:44:49 +0200

    login-form

---
+++ src/views/LoginWiew.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <Login/>
+  </div>
+</template>
+
+<script>
+import Login from "@/components/Login.vue";
+
+export default {
+  name: 'login-View',
+  components: {
+    Login,
+  }
+}
+</script>
